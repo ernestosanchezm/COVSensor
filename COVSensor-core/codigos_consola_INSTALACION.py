919ba3de59296ac108330162050267915de8c1de
Ernesto Sanchez gitlab generico <ernesto.sm.95@hotmail.com>
2022-04-22 07:36:11 -0500

    _

---
+++ COVSensor-core/codigos_consola_INSTALACION.py
@@ -0,0 +1,19 @@
+https://www.youtube.com/watch?v=XRSX6eo7BEA&ab_channel=G%C3%B6khanAtal%C4%B1
+
+wget https://github.com/Gadgetoid/py-spidev/archive/master.zip
+unzip master.zip
+rm master.zip
+cd py-spidev-master
+sudo python setup.py install
+sudo python3 setup.py install
+cd ..
+cd Desktop
+mkdir NRF24L01
+cd NRF24L01
+git clone https://github.com/BLavery/lib_nrf24   //reemplazar esta libreria por la actual para raspberry pi 4 :: https://github.com/trueToastedCode/Python/blob/master/Fix/lib_nrf24.py
+cd lib_nrf24/
+cp lib_nrf24.py ~/Desktop/NRF24L01  
+sudo nano hbr.py
+git clone https://github.com/TMRh20/RF24
+
+
