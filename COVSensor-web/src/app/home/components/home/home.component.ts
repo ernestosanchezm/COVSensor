e7fd35658573b69c0213bbcba9800fa226b26e77
MiguelOre <maoq_2009@hotmail.com>
2022-05-27 19:30:03 -0500

    changes to build

--- COVSensor-web/src/app/home/components/home/home.component.ts
+++ COVSensor-web/src/app/home/components/home/home.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+
+  image1 = "assets/oficina.webp";
+  image2 = "assets/sensored.webp";
+
   constructor(private location: Location) {}
 
   ngOnInit(): void {}
--- COVSensor-web/src/environments/environment.prod.ts
+++ COVSensor-web/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:5000',
+  socketurl: 'http://localhost:3000',
 };
