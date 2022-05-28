b1aa85aedee21bc234e102408f015665ed050318
MiguelOre <maoq_2009@hotmail.com>
2022-05-27 19:37:01 -0500

    changues home

--- COVSensor-web/src/app/home/components/home/home.component.ts
+++ COVSensor-web/src/app/home/components/home/home.component.ts
@@ -10,6 +10,7 @@ export class HomeComponent implements OnInit {
 
   image1 = "assets/oficina.webp";
   image2 = "assets/sensored.webp";
+  image3 = "assets/medidor.jpg";
 
   constructor(private location: Location) {}
 
